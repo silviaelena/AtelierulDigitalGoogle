0ba2ce5dfba4a50aeeb8ec672037f6968ed17d55
silvianistor <silvia.elena110@gmail.com>
2022-12-14 19:19:57 +0200

    concurrent modification exception

--- src/collections/Main.java
+++ src/collections/Main.java
@@ -1,11 +1,12 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
-    public static void useArrayList() {
+    public static List getList() {
         List list = new LinkedList();
         list.add("First");
         list.add("Second");
@@ -13,8 +14,19 @@ public class Main {
         list.add("Empty");
         list.add("Something");
         System.out.println(list);
+        return list;
     }
     public static void main(String[] args) {
-        useArrayList();
+        List a = getList();
+        Iterator<String> it = a.iterator();
+
+        while(it.hasNext()) {
+            if (it.next() == "Third") {
+                it.remove();
+//                a.remove(2);
+            }
+        }
+
+        System.out.println(a);
     }
 }
