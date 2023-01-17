4ed76cd25067a68767a1f02cad8e7a5ba231067f
silvianistor <silvia.elena110@gmail.com>
2023-01-17 21:23:28 +0200

    Added exceptions exercise done in teams

--- src/phonebook/Main.java
+++ src/phonebook/Main.java
@@ -1,13 +1,33 @@
 package phonebook;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static String findUser() {
+    public static void findUser() {
         BufferedReader reader;
-//        try {
-//
-//        }
-        return "Ana";
+        boolean foundUsers = false;
+
+        try {
+            reader = new BufferedReader(new FileReader("/Users/silvianistor/IdeaProjects/AtelierulDigitalGoogle/src/phonebook/phonebook.txt"));
+            String line = reader.readLine();
+            while (line != null && !foundUsers) {
+                String[] tokens = line.split(" ");
+                if (tokens[0].equals("Abbey"))
+                    System.out.print(tokens[1] + ", ");
+                if (tokens[0].equals("Abdul")) {
+                    foundUsers = true;
+                    System.out.print(tokens[1]);
+                }
+                line = reader.readLine();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        findUser();
     }
 }
